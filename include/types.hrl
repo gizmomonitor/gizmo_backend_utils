7ffeb56402dec0aa79539953128d3dc6d82ef2ed
Mateusz Korszun <mkorszun@gmail.com>
2013-07-13 21:49:19 +0200

    More type conversion macros

--- include/types.hrl
+++ include/types.hrl
@@ -1,7 +1,7 @@
--define(L2B(L), list_to_binary(L)).
--define(I2B(Int), list_to_binary(integer_to_list(Int))).
--define(L2A(L), try list_to_existing_atom(L) catch _:_ -> list_to_atom(L) end).
--define(B2I(B), list_to_integer(binary_to_list(B))).
+-define(I2B(I), list_to_binary(integer_to_list(I))).
+
+-define(B2I(B), try list_to_integer(binary_to_list(B)) catch _:_ -> undefined end).
 -define(B2A(Bin), ?L2A(binary_to_list(Bin))).
--define(B2T(B), binary_to_term(B)).
+
 -define(L2I(L), try list_to_integer(L) of I -> I catch _:_ -> undefined end).
+-define(L2A(L), try list_to_existing_atom(L) catch _:_ -> list_to_atom(L) end).
